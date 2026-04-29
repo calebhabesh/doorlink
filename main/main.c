ec328accf78652032238dab77c5e452c2f339542
Caleb Habesh <87959618+calebhabesh@users.noreply.github.com>
2026-04-29 18:30:58 -0400

    chore: setup esp-idf firmware placeholders

--- main/main.c
+++ main/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG = "smart_doorbell";
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "Initializing Smart Doorbell System");
 
+    // TODO: Initialize WiFi Station
+    ESP_LOGI(TAG, "[Placeholder] WiFi Init");
+
+    // TODO: Initialize Camera (OV5640)
+    ESP_LOGI(TAG, "[Placeholder] Camera Init");
+
+    // TODO: Initialize I2S (INMP441 & MAX98357A)
+    ESP_LOGI(TAG, "[Placeholder] I2S Audio Init");
+
+    // TODO: Setup Button Interrupts
+    ESP_LOGI(TAG, "[Placeholder] Button Init");
+
+    // TODO: Connect to MQTT Broker
+    ESP_LOGI(TAG, "[Placeholder] MQTT Init");
+
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
