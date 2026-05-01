7451b8aa1623f0de7d85fe3b08f1b13ee99a26a9
Caleb Habesh <87959618+calebhabesh@users.noreply.github.com>
2026-05-01 18:20:35 -0400

    feat: create Event entity and repository

---
+++ gateway/src/test/java/com/smartdoorbell/gateway/repository/EventRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.smartdoorbell.gateway.repository;
+
+import com.smartdoorbell.gateway.entity.Event;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest(properties = {
+    "spring.datasource.url=jdbc:postgresql://localhost:5432/smartdoorbell",
+    "spring.datasource.username=user",
+    "spring.datasource.password=password"
+})
+public class EventRepositoryTest {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Test
+    public void testSaveEvent() {
+        Event event = new Event(LocalDateTime.now(), "DOORBELL_PRESS", "doorbell-images/test.jpg");
+        Event saved = eventRepository.save(event);
+        assertNotNull(saved.getId());
+    }
+}
